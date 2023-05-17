fe1315865567466104d5c8fb2996b1bc590d95d8
FoxyRaspberry <hogwartsgsrh97@gmail.comm>
2023-05-17 17:57:21 +0300

    feat: change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+  ],
+};
