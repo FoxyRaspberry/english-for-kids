01930f6c433678d0b9d35ea08b4980df45bd8c44
FoxyRaspberry <hogwartsgsrh97@gmail.comm>
2023-05-17 22:11:36 +0300

    feat: use `webpack-dev-server`

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.join(__dirname, 'src', 'index.js'),
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
       filename: 'index.html',
     }),
   ],
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    port: 9000,
+  },
 };
