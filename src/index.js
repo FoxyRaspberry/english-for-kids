b05fecebebb8c49caa869f78de63fad3f7f48186
FoxyRaspberry <hogwartsgsrh97@gmail.comm>
2023-05-18 11:25:26 +0300

    refactor: delete the useless code snippets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './style.css';
-import './images/bg.png';
-import './images/icon_home.png';
-import './images/icon_menu.png';
-
-function component(text) {
-  const element = document.createElement('h1');
-  element.textContent = text;
-  return element;
-}
-
-document.body.prepend(component('Проект собран на Webpack'));
