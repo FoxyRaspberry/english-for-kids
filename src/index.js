50eb37f7a40fc5302ab580c146c5e3dc249d6236
FoxyRaspberry <hogwartsgsrh97@gmail.comm>
2023-05-23 00:03:16 +0300

    feat: create `displayCategoryCards`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 import { categoriesMap } from './data/categories';
 import { createWordCategoryCard } from './assets/word-category-card/word-category-card';
+
+function displayCategoryCards(categoriesMap, cardsContainerElement) {
+  const cardsDocumentFragment = new DocumentFragment();
+  categoriesMap.forEach(categories => {
+    const categoriesCardElement = createWordCategoryCard(categories);
+    cardsDocumentFragment.appendChild(categoriesCardElement);
+  });
+  cardsContainerElement.appendChild(cardsDocumentFragment);
+}
+
+const catalogCardsContainerElement = document.getElementsByClassName('word-category-card__container')[0];
+displayCategoryCards (categoriesMap, catalogCardsContainerElement);
